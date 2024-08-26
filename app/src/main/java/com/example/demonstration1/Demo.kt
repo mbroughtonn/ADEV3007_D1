43c621721110bc41c9feccb3133cffd0df72a7e9
mbroughtonn <111618876+mbroughtonn@users.noreply.github.com>
2024-08-26 09:50:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/demonstration1/Demo.kt
@@ -0,0 +1,34 @@
+package com.example.demonstration1
+
+fun main(){
+
+    // Single line comment
+
+    /*
+        Multi line comment
+    */
+
+    println("Hello World")
+
+    // Variable declaration
+    val num = 10 // Read only variable
+
+    var num2 = 33 // Writable variable aka mutable
+
+    /*
+    DATA TYPES -
+    Primitives (Simple Types)
+        Integer, Double, Boolean
+    Non-primitive
+        String, Array Classes
+     */
+    val multiLineString = """
+        This is line 1
+        This is line 2
+        This is line 3
+    """
+    println(multiLineString)
+
+    val PI = 3.1452
+    println(PI)
+}
